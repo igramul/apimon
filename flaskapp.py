f74c3a01881247f9f4f87dc2d9ad3db14b69666a
Lukas Burger <lukas.burger@igramul.ch>
2025-02-12 22:22:40 +0100

    add neopixel stripe

--- flaskapp.py
+++ flaskapp.py
@@ -9,10 +9,15 @@ from oauthlib.oauth2 import BackendApplicationClient
 from requests.auth import HTTPBasicAuth
 from requests_oauthlib import OAuth2Session
 
+import board
+import neopixel
+
 import version
 
 # Load environment variables from .env file
 load_dotenv()
+
+pixels = neopixel.NeoPixel(board.D18, 30)
 
 tickets = {}
 
@@ -38,7 +43,7 @@ def get_root():
         'tickets': tickets
     })
 
-
+status_list = ['Open', 'In Progress', 'Deferred', 'Checking']
 def _update_tickets():
     token_url = os.environ.get('ACCESS_TOKEN_URL')
     client_id = os.environ.get('CLIENT_ID')
@@ -62,8 +67,6 @@ def _update_tickets():
 
     url = f'{base_url}/{path}'
 
-    status_list = ['Open', 'In Progress', 'Deferred', 'Checking']
-
     for status in status_list:
 
         jql = f'project = AITG AND component = APIM-Betrieb AND status = "{status}"'
@@ -79,9 +82,50 @@ def _update_tickets():
         tickets[status] = response_json.get('total')
 
 
+color_weiss = (255,255,255)
+color_black = (0,0,0)
+color_red = (255,0,0)
+color_green = (0,255,0)
+color_blue = (0,0,255)
+color_magenta = (255,0,255)
+
+pixel_array = [color_black]
+def _update_pixels():
+    index = 0
+    if pixel_array[0] == color_weiss:
+        pixel_array[0] = color_black
+    else:
+         pixel_array[0] = color_weiss
+    pixels[index] = pixel_array[0]
+    index =+ 1
+    for status in reversed(status_list):
+        count = tickets[status]
+        if status == 'Checking':
+            color = color_green
+        if status == 'Deferred':
+            color = color_blue
+        if status == 'In Progress':
+            color = color_magenta
+        if status == 'Open':
+            color = color_red
+        for i in range(count):
+            pixels[index+i] = color
+        index += count
+        for i in range(index, pixels.n):
+            pixels[i] = color_black
+    pixels.show()
+
+
 @scheduler.task('cron', id='do_job_update_tickets', minute='*/5')
 def job_update_tickets():
     _update_tickets()
+
+
+@scheduler.task('interval', id='do_job_update_pixels', seconds=1)
+def job_update_pixels():
+   _update_pixels() 
+
+
 
 _update_tickets()
 
